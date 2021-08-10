be9b5475a87f68ba51082d1b182452813131cd40
Piyush Kadam <kadam.piyush.17it1041@gmail.com>
2021-08-10 19:56:14 +0530

    UC11_object_creation

---
+++ uc11.js
@@ -0,0 +1,41 @@
+var con = require("./constants.js")
+
+console.log("Welcome to Employee Wage computation");
+
+let emp_check = Math.floor(Math.random() * 10) % 2;
+
+if (emp_check == con.is_Absent){
+    console.log("Employee is absent");
+}
+else{
+    console.log("Employee is present");
+}
+
+let empobj = [];
+var empwage = 0;
+var empwage1 = 0;
+var time = 0;
+var day = 0;
+while(time <= con.time && day < con.days){
+    const empcheck = Math.floor(Math.random() * 10) % 3;
+    let emphr = 0;
+
+    switch(empcheck){
+        case 0: emphr = con.fulltime;   //fulltime
+                break;
+        case 1: emphr = con.parttime;   //partime
+                break;
+        default: emphr = 0;
+                break;
+    }
+    day++;
+    time += emphr;
+    empwage = emphr * con.wageperhr; //time * wage per hr
+    empwage1 += empwage;
+    empobj.push({
+        'Day': day,
+        'Hours': emphr,
+        'Wage': empwage
+    })
+}
+console.log(empobj);
