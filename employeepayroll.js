9d22f09602ee19166223c07d5bfa96cdf791dd5f
Piyush Kadam <kadam.piyush.17it1041@gmail.com>
2021-08-11 20:01:32 +0530

    Employee_payroll_with_validation

--- employeepayroll.js
+++ employeepayroll.js
@@ -22,6 +22,44 @@ class employee{
     display() {
         console.log('Id :',this.id,", Name :",this.name,', Salary :',this.salary,', Date of joining :',this.doj,', Gender :',this.gender);
     }
+
+    validation(){
+        var mobile = "91 9876543210";
+        var regx = /^[\d]{2}[\s][6789][\d]{9}$/;
+        if ( regx.test(mobile) == true ){
+            console.log("Valid");
+        }
+        else{
+            throw "Not Valid";
+        }
+
+        var email = "abc@gmail.com";
+        var regx = /^([A-Za-z0-9._]+)(@)([\w]+)(.)([\w]{3})(.)?([\w]){2,3}?$/;
+        if ( regx.test(email) == true ){
+            console.log("Valid");
+        }
+        else{
+            throw "Not Valid";
+        }
+
+        var uname = "Pk123";
+        var regx = /^([A-Z])([\w]){1}([\w])+$/;
+        if ( regx.test(uname) == true ){
+            console.log("Valid");
+        }
+        else{
+            throw "Not Valid";
+        }
+
+        var gender = "M";
+        var regx = /^([MF])$/;
+        if ( regx.test(gender) == true ){
+            console.log("Valid");
+        }
+        else{
+            throw "Not Valid";
+        }
+    }
 }
   
 let a = new employee(1,'ABC',5000);
@@ -32,3 +70,4 @@ a.setSal(10000);
 a.setId(2);
 a.details('09/08/21','Female');
 a.display();
+a.validation();
